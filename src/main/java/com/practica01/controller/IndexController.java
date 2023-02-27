9cdc57f6a2ff63e9729d3a45d7bf4d3d8a3564de
Jeanca Barahona <Jeanca Barahona@DESKTOP-7U4I28G>
2023-02-27 01:23:36 -0600

    parte 1 BD Y modificar partes del css y html Hecho por Jean Carlo Barahona

--- src/main/java/com/practica01/controller/IndexController.java
+++ src/main/java/com/practica01/controller/IndexController.java
@@ -1,14 +1,25 @@
 package com.practica01.controller;
 
+import com.practica01.dao.EstadoDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
     
+    @Autowired
+    EstadoDao estadoDao;
+    
     @GetMapping("/")
     public String inicio(Model model) {
+        log.info("Estamos Utilizando MVC");
+        
+        var estados = estadoDao.findAll();
+        model.addAttribute("estados",estados);
         return "index";
     }
     
---
+++ src/main/java/com/practica01/dao/EstadoDao.java
@@ -0,0 +1,8 @@
+package com.practica01.dao;
+
+import com.practica01.domain.estado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstadoDao extends CrudRepository<estado, Long>{
+    
+}
