6a91053a222a5b66bdb1d4c09487d61a0cb2ede8
Jeanca Barahona <Jeanca Barahona@DESKTOP-7U4I28G>
2023-02-27 15:02:23 -0600

    Actualizacion de parte IV 2.0

--- src/main/java/com/practica01/service/EstadoServiceImpl.java
+++ src/main/java/com/practica01/service/EstadoServiceImpl.java
@@ -4,9 +4,10 @@ import com.practica01.dao.EstadoDao;
 import com.practica01.domain.Estado;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Service
 public class EstadoServiceImpl implements EstadoService {
     
     @Autowired
